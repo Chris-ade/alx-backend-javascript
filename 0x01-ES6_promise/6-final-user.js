38967c802c9e780af6576b237865b90503fb396b
Chris Ade <101924502+Chris-ade@users.noreply.github.com>
2023-01-24 19:27:07 +0100

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,16 @@
 /* eslint-disable import/extensions */
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  try {
-    const [user, photo] = await Promise.all([
-      signUpUser(firstName, lastName),
-      uploadPhoto(fileName),
-    ]);
-    return [{ status: 'fulfilled', value: user }, { status: 'fulfilled', value: photo }];
-  } catch (err) {
-    return [{ status: 'rejected', value: err }];
-  }
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
+  return results.map((pr) => ({
+    status: pr.status,
+    value: pr.status === 'fulfilled' ? pr.value : String(pr.reason),
+  }));
 }
+
+export default handleProfileSignup;
