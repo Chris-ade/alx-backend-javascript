92ca716af011e77855fbd86865a3fd1e4294ad78
Chris Adebiyi <joeade12054@gmail.com>
2023-01-24 08:49:35 -0800

    Task done

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/extensions */
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const [user, photo] = await Promise.all([
+      signUpUser(firstName, lastName),
+      uploadPhoto(fileName),
+    ]);
+    return [{ status: 'fulfilled', value: user }, { status: 'fulfilled', value: photo }];
+  } catch (err) {
+    return [{ status: 'rejected', value: err }];
+  }
+}
