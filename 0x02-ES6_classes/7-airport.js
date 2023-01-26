7de378d45aed83ae2171646c31cbd4b9a926ff3f
Chris Adebiyi <joeade12054@gmail.com>
2023-01-26 15:33:23 -0800

    Task done

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,29 +1,29 @@
 /* eslint no-underscore-dangle: 0 */
 class Airport {
-    constructor(name, code) {
-        this._name = name;
-        this._code = code;
-    }
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
 
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    set name(value) {
-        this._name = value;
-    }
+  set name(value) {
+    this._name = value;
+  }
 
-    get code() {
-        return this._code;
-    }
+  get code() {
+    return this._code;
+  }
 
-    set code(value) {
-        this._code = value;
-    }
+  set code(value) {
+    this._code = value;
+  }
 
-    toString() {
-        return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
-    }
+  toString() {
+    return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+  }
 }
 
 export default Airport;
