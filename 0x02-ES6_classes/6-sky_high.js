77b06dbcc710ef0bc4a092f71ce6af8fac1d4165
Chris Adebiyi <joeade12054@gmail.com>
2023-01-26 15:26:36 -0800

    Task done

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,22 +1,25 @@
 /* eslint no-underscore-dangle: 0 */
 import Building from './5-building';
 
-class SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    if (typeof floors !== 'number') {
-      throw new Error('floors must be a number');
-    }
     this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
   }
 
   get floors() {
     return this._floors;
   }
 
+  set floors(floors) {
+    this._floors = floors;
+  }
+
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
-
-export default SkyHighBuilding;
