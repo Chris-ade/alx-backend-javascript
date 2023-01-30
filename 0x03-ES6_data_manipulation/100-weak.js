5352a04331cdd35edfc4dfd26533fce83b3638c1
Chris Ade <101924502+Chris-ade@users.noreply.github.com>
2023-01-30 22:35:37 +0100

    Update 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,16 +1,11 @@
-const weakMap = new WeakMap();
+export const weakMap = new WeakMap();
 
-const queryAPI = (endpoint) => {
+export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 1);
-  } else {
-    let count = weakMap.get(endpoint);
-    weakMap.set(endpoint, ++count);
+    weakMap.set(endpoint, 0);
   }
-
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
   if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
-};
-
-export { weakMap, queryAPI };
+}
