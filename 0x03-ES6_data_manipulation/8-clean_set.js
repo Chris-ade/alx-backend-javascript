5cb81935bdcc94b78cab1f254690969f6e9d6f9f
Chris Adebiyi <joeade12054@gmail.com>
2023-01-30 13:13:03 -0800

    Task done

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
-  let result = "";
+  let result = '';
   for (const value of set) {
     if (value.startsWith(startString)) {
-      result += value.slice(startString.length) + "-";
+      result += `${value.slice(startString.length)}-`;
     }
   }
   return result.slice(0, -1);
